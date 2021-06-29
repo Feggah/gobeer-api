f9e83c658b5c04f92535cc57ef0582d53016c944
Feggah <gabidferreira9@gmail.com>
2021-06-29 20:14:32 -0300

    Add services implementation
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,5 +1,16 @@
 package beer
 
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/pkg/errors"
+)
+
+const (
+	errInvalidID = "Invalid ID"
+)
+
 type UseCase interface {
 	GetAll() ([]*Beer, error)
 	Get(ID int64) (*Beer, error)
@@ -8,28 +19,109 @@ type UseCase interface {
 	Remove(ID int64) error
 }
 
-type Service struct{}
-
-func NewService() *Service {
-	return &Service{}
+type Service struct {
+	DB *sql.DB
 }
 
-func (s *Service) GetAll() ([]*Beer, error) {
-	return nil, nil
+func NewService(db *sql.DB) *Service {
+	return &Service{
+		DB: db,
+	}
 }
 
-func (s *Service) Get(ID int64) (*Beer, error) {
-	return nil, nil
+func (svc *Service) GetAll() ([]*Beer, error) {
+	var result []*Beer
+
+	rows, err := svc.DB.Query("SELECT id, name, type, style from beer")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var beer Beer
+		err = rows.Scan(&beer.ID, &beer.Name, &beer.Type, &beer.Style)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, &beer)
+	}
+	return result, nil
 }
 
-func (s *Service) Store(beer *Beer) error {
-	nil
+func (svc *Service) Get(ID int64) (*Beer, error) {
+	var beer Beer
+
+	stmt, err := svc.DB.Prepare("select id, name, type, style from beer where id = ?")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+
+	err = stmt.QueryRow(ID).Scan(&beer.ID, &beer.Name, &beer.Type, &beer.Style)
+	if err != nil {
+		return nil, err
+	}
+	return &beer, nil
 }
 
-func (s *Service) Update(beer *Beer) error {
-	nil
+func (svc *Service) Store(beer *Beer) error {
+	tx, err := svc.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("insert into beer(id, name, type, style) values (?,?,?,?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(beer.ID, beer.Name, beer.Type, beer.Style)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
 }
 
-func (s *Service) Remove(ID int64) error {
-	nil
+func (svc *Service) Update(beer *Beer) error {
+	if beer.ID == 0 {
+		return errors.New(errInvalidID)
+	}
+
+	tx, err := svc.DB.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("update beer set name=?, type=?, style=? where id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(beer.Name, beer.Type, beer.Style, beer.ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
+}
+
+func (svc *Service) Remove(ID int64) error {
+	if ID == 0 {
+		return errors.New(errInvalidID)
+	}
+
+	tx, err := svc.DB.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("delete from beer where id=?", ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	tx.Commit()
+	return nil
 }
