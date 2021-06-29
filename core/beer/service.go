147f3a523915fce6d24e26a7d866df95994b1740
Feggah <gabidferreira9@gmail.com>
2021-06-29 16:13:24 -0300

    Add usecases and service scaffolding
    Signed-off-by: Feggah <gabidferreira9@gmail.com>

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,1 +1,35 @@
 package beer
+
+type UseCase interface {
+	GetAll() ([]*Beer, error)
+	Get(ID int64) (*Beer, error)
+	Store(beer *Beer) error
+	Update(beer *Beer) error
+	Remove(ID int64) error
+}
+
+type Service struct{}
+
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetAll() ([]*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Get(ID int64) (*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Store(beer *Beer) error {
+	nil
+}
+
+func (s *Service) Update(beer *Beer) error {
+	nil
+}
+
+func (s *Service) Remove(ID int64) error {
+	nil
+}
